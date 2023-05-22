b18b808e86ef701dcf0c04bafd55237c99bb173f
SergeiAnishchenko <79313219184@yandex.ru>
2023-05-22 20:40:31 +0300

    Мастер-ветка по SpringWeb.

---
+++ src/main/java/ru/skypro/lessons/springboot/myemployeeweblibrary/MyEmployeeWebLibraryApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.myemployeeweblibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyEmployeeWebLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyEmployeeWebLibraryApplication.class, args);
+    }
+
+}
